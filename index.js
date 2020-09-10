d72068e844faa5f8c4a6ed9cd75e8f83ef9c96c0
pohodnik58 <pohodnik58@gmail.com>
2020-09-10 09:42:01 +0300

    import resolve

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-module.exports = function () {
-    return {
-        presets: [
-            ['@babel/preset-env', { modules: false, useBuiltIns: 'usage', corejs: 3 }],
-        ],
+// eslint-disable-next-line global-require,import/no-dynamic-require
+const use = pkg => require(pkg).default;
 
-        plugins: [
-            ['@babel/plugin-proposal-class-properties', { loose: true }],
-            ['@babel/plugin-transform-runtime'],
-            ['@babel/plugin-proposal-export-default-from'],
-            ['@babel/plugin-syntax-dynamic-import'],
-            ['@babel/plugin-proposal-optional-chaining'],
-            ['@babel/plugin-transform-regenerator', { async: false }]
-        ],
-        env: {
-            test: {
-                presets: [
-                    ['@babel/preset-env', { modules: 'commonjs', targets: { node: 'current', esmodules: false } }]
-                ]
-            },
-            development: {
-                plugins: []
-            },
-            production: {
-                presets: ['minify', { removeConsole: true, removeDebugger: true }]
-            }
+module.exports = () => ({
+    presets: [
+        [use('@babel/preset-env'), { modules: false, useBuiltIns: 'usage', corejs: 3 }],
+    ],
+
+    plugins: [
+        [use('@babel/plugin-proposal-class-properties'), { loose: true }],
+        [use('@babel/plugin-transform-runtime')],
+        [use('@babel/plugin-proposal-export-default-from')],
+        [use('@babel/plugin-syntax-dynamic-import')],
+        [use('@babel/plugin-proposal-optional-chaining')],
+        [use('@babel/plugin-transform-regenerator'), { async: false }]
+    ],
+    env: {
+        test: {
+            presets: [
+                [use('@babel/preset-env'), { modules: 'commonjs', targets: { node: 'current', esmodules: false } }]
+            ]
+        },
+        development: {
+            plugins: []
+        },
+        production: {
+            presets: [use('minify'), { removeConsole: true, removeDebugger: true }]
         }
-    };
-};
+    }
+});
